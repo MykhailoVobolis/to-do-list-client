7a6b96cd10afcd0fde71a597301962b850bfe139
MykhailoVobolis <mvobolis@gmail.com>
2025-01-13 16:51:32 +0200

    added loader

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,11 +2,16 @@ import { useEffect } from "react";
 import Layout from "../Layout/Layout.jsx";
 import TaskForm from "../TaskForm/TaskForm.jsx";
 import TaskList from "../TaskList/TaskList.jsx";
+import Loader from "../Loader/Loader.jsx";
 import { fetchTasks } from "../../redux/tasks/operations.js";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { selectError, selectLoading } from "../../redux/tasks/selectors.js";
+import Error from "../Error/Error.jsx";
 
 export default function App() {
   const dispatch = useDispatch();
+  const loading = useSelector(selectLoading);
+  const error = useSelector(selectError);
 
   useEffect(() => {
     dispatch(fetchTasks());
@@ -16,6 +21,8 @@ export default function App() {
     <Layout>
       <h1>To Do List</h1>
       <TaskForm />
+      {loading && <Loader />}
+      {error && <Error>Sorry, an error occurred! Try reloading this page!</Error>}
       <TaskList />
     </Layout>
   );
--- src/redux/tasks/selectors.js
+++ src/redux/tasks/selectors.js
@@ -1,3 +1,5 @@
 export const selectLoading = (state) => state.tasks.loading;
 
+export const selectError = (state) => state.tasks.error;
+
 export const selectTasks = (state) => state.tasks.items;
