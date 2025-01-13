c7eedfbadce380e6217e396a6b3ffa1074f2b743
MykhailoVobolis <mvobolis@gmail.com>
2025-01-13 19:45:26 +0200

    fix intrface

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -5,7 +5,7 @@ import TaskList from "../TaskList/TaskList.jsx";
 import Loader from "../Loader/Loader.jsx";
 import { fetchTasks } from "../../redux/tasks/operations.js";
 import { useDispatch, useSelector } from "react-redux";
-import { selectError, selectLoading } from "../../redux/tasks/selectors.js";
+import { selectError, selectLoading, selectTasks } from "../../redux/tasks/selectors.js";
 import Error from "../Error/Error.jsx";
 import TextFilter from "../TextFilter/TextFilter.jsx";
 import { selectTextFilter } from "../../redux/filters/selectors.js";
@@ -15,6 +15,7 @@ export default function App() {
   const loading = useSelector(selectLoading);
   const error = useSelector(selectError);
   const filterText = useSelector(selectTextFilter);
+  const tasks = useSelector(selectTasks);
 
   useEffect(() => {
     dispatch(fetchTasks(filterText));
@@ -24,10 +25,10 @@ export default function App() {
     <Layout>
       <h1>To Do List</h1>
       <TaskForm />
-      <TextFilter />
+      {tasks.length > 0 && <TextFilter />}
       {loading && <Loader />}
       {error && <Error>Sorry, an error occurred! Try reloading this page!</Error>}
-      <TaskList />
+      {tasks.length > 0 && <TaskList />}
     </Layout>
   );
 }
